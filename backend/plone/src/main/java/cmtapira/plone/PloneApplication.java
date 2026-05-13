1e5d6665072ab356cd97dedda9640349184b4a62
gustavokrm <gustavokrm@gmail.com>
2026-05-13 09:30:24 -0300

    Trabalho inicial na criação do backend

---
+++ backend/plone/src/main/java/cmtapira/plone/PloneApplication.java
@@ -0,0 +1,13 @@
+package cmtapira.plone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PloneApplication.class, args);
+	}
+
+}
---
+++ backend/plone/src/test/java/cmtapira/plone/PloneApplicationTests.java
@@ -0,0 +1,13 @@
+package cmtapira.plone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
